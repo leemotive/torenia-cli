8b9333387a108385048edd9a16ccb6ebaf1e5f3a
leemotive <wstation@163.com>
2018-11-07 20:07:54 +0800

    添加加载小动画

--- command/create.js
+++ command/create.js
@@ -3,6 +3,7 @@ const walk = require('walk');
 const fs = require('fs');
 const fse = require('fs-extra');
 const ejs = require('ejs');
+const colors = require('colors');
 
 const cwd = process.cwd();
 const templateRoot = path.resolve(__dirname, '../template');
@@ -15,10 +16,21 @@ module.exports = function (data) {
 
   fse.ensureDirSync(projectRoot);
 
-  const walker = walk.walk(path.resolve(templateRoot));
   let hasRejected = false;
+  let walkend = false;
+
+  const spinner = '⠋⠙⠹⠸⠼⠴⠦⠧⠇⠏';
+  let count = 0;
+  function spin() {
+    process.stdout.write(`\r processing ${spinner[count++ % 10]} `);
+    if (!hasRejected && (count < 30 || !walkend)) {
+      setTimeout(spin, 100);
+    }
+  }
+  spin();
 
 
+  const walker = walk.walk(path.resolve(templateRoot));
   walker.on('file', (root, fileStats, next) => {
     const filename = fileStats.name;
     const newRoot = root.replace(templateRoot, projectRoot);
@@ -59,17 +71,19 @@ module.exports = function (data) {
       hasRejected = true;
     }
     hasRejected || next();
-  })
+  });
+
+  walker.on('end', () => {
+    walkend = true;
+  });
 
   process.on('exit', function() {
     if (hasRejected) {
       fse.removeSync(projectRoot);
-      console.error('create project failed');
+      console.error(colors.red('\rcreate project failed'));
     } else {
-      console.log('Create project successfully');
+      console.log('\rCreate project successfully');
     }
   });
 }
-
-
 
